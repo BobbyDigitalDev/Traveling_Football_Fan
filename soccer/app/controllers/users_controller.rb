b91af38755ac0fb745db4005cc9590a56c18cf02
Michael Stokes <mjstokes1986@att.net>
2015-05-26 15:19:46 -0400

    Sign up page html

--- soccer/app/controllers/users_controller.rb
+++ soccer/app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  	render :new
   end
+  
+
+  def create
+  	@user = User.new
+  	@user.name = params[:name]
+  	@user.email = params[:email]
+  	@user.password_digest = params[:password]
+  	@user.password_digest = params[:password]
+
+  	if @user.save 
+  		redirect_to "/users" #come and modify when itineraries are made
+  	else
+  		render :new
+  	end
+
+
+
+
+  end
+
+
 
   # def edit
   # end
