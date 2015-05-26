75e495056996a97c346fd5b44db21577614624ab
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-05-26 14:01:16 -0400

    logon

--- soccer/app/controllers/sessions_controller.rb
+++ soccer/app/controllers/sessions_controller.rb
@@ -1,9 +1,30 @@
 class SessionsController < ApplicationController
+  
   def new
+    render :new
   end
 
-  def show
-    @user = User.find(session[:user_id])
-    render :edit
+  def create 
+
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+
+      session[:user_id] = user.id 
+
+      redirect_to edit_users_path 
+    else 
+
+      @message ="This email and password combination does not exist."
+      render :new
+    end
   end
+
+  def destroy 
+
+    session[:user_id] = nil 
+
+    redirect_to new_sessions_path 
+  end 
+
 end
--- soccer/app/controllers/users_controller.rb
+++ soccer/app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def new
   end
 
-  def edit
-  end
+  # def edit
+  # end
+
+
 end
