5d51aa805f763e688a911414cacb950c62aa1dfb
Bobby Lopez <bobbyl76@gmail.com>
2015-05-26 17:28:13 -0400

    password validation working

--- soccer/app/controllers/users_controller.rb
+++ soccer/app/controllers/users_controller.rb
@@ -10,26 +10,12 @@ class UsersController < ApplicationController
   	@user.name = params[:name]
   	@user.email = params[:email]
   	@user.password = params[:password]
+    @user.password_confirmation = params[:password_confirmation]
 
-    if params[:password] != params[:password_confirmation]
-      flash.now[:alert] = "Passwords must match!"
-    end
-
-
-  	if @user.save 
+    if @user.save 
   		redirect_to "/users" #come and modify when itineraries are made
   	else
   		render :new
-  	end
+    end
   end
-
-
-
-  # def edit
-  # end
-
-  def create
-  end
-
-
 end
--- soccer/app/models/user.rb
+++ soccer/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	self.has_secure_password()
+  # The below happens automatically by calling has_secure_password
+  # self.validates(:password, {confirmation: true})
 end
