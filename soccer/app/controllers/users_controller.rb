6cd79be721b8abeb1b84d49d50dff5c198a2c5d0
Michael Stokes <mjstokes1986@att.net>
2015-05-27 10:56:09 -0400

    checkpoint 10:56am

--- soccer/app/controllers/users_controller.rb
+++ soccer/app/controllers/users_controller.rb
@@ -1,30 +1,29 @@
 class UsersController < ApplicationController
+
+  
   def new
-  	@user = User.new
-  	render :new
+    @user = User.new
+    render :new
   end
   
 
   def create
-  	@user = User.new
-  	@user.name = params[:name]
-  	@user.email = params[:email]
-  	@user.password = params[:password]
+    @user = User.new
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
     @user.password_confirmation = params[:password_confirmation]
 
-<<<<<<< HEAD
-    if params[:password] == params[:password_confirmation]
+  if params[:password] == params[:password_confirmation]
       flash.now[:alert] = "Passwords must match!"
-    end
-x
+  end
 
-  	if @user.save 
-=======
+
     if @user.save 
->>>>>>> a014b5be7bd203697a668c1b5b368bdfe2b70f86
-  		redirect_to "/users" #come and modify when itineraries are made
-  	else
-  		render :new
+      #redirect_to "/users" 
+       redirect_to "/"#come and modify when itineraries are made
+    else
+      render :new
     end
   end
 
