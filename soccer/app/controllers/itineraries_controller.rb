a693e65a2a08f7e4dd6ffcd0eb75fd82979d02d5
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-05-27 16:16:48 -0400

    blahhh

--- soccer/app/controllers/itineraries_controller.rb
+++ soccer/app/controllers/itineraries_controller.rb
@@ -1,28 +1,28 @@
 class ItinerariesController < ApplicationController
-  def index
-    if session[:user_id] != nil # the client is logged in
-      user = User.find(session[:user_id])
-      itineraries = Itinerary.where({user_id: user.id})
-      # render text: "Hello, I know who you are: #{user.email}"
-      render :index
-      #we need to cr
-    else
-      #render text: 'Woah', status: 401
-      redirect_to '/'
-    end
-    # user = User.find_by(email: params[:email])
-    # puts User.authenticate 
-    # if user && user.authenticate(params[:password])
-    #   session[:user_id] = user.id 
-    # else
-    #   @message ="This email and password combination does not exist."
-    #   render :index
-    # end
-  end
+ def index
+   if session[:user_id] != nil # the client is logged in
+     @user = User.find(session[:user_id])
+     @itinerary = Itinerary.find( @user.id )
+     @itinerary = Itinerary.where({user_id: @user.id})
+     # render text: "Hello, I know who you are: #{user.email}"
+     render :index
+     #we need to cr
+   else
+     #render text: 'Woah', status: 401
+     redirect_to '/'
+   end
+   # user = User.find_by(email: params[:email])
+   # puts User.authenticate 
+   # if user && user.authenticate(params[:password])
+   #   session[:user_id] = user.id 
+   # else
+   #   @message ="This email and password combination does not exist."
+   #   render :index
+   # end
+ end
 
 
-
-  def show
-  end
+ def show
+ end
 end
 
