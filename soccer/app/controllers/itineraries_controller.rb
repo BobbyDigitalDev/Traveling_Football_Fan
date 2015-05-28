c7f0f8d1bff1c68521c267c53ae21b9e085838e4
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-05-28 10:14:43 -0400

    editsave

--- soccer/app/controllers/itineraries_controller.rb
+++ soccer/app/controllers/itineraries_controller.rb
@@ -1,29 +1,7 @@
 class ItinerariesController < ApplicationController
 
-<<<<<<< HEAD
- def index
-   if session[:user_id] != nil # the client is logged in
-     @user = User.find(session[:user_id])
-     @itinerary = Itinerary.where({user_id: @user.id})
-     # render text: "Hello, I know who you are: #{user.email}"
-     render :index
-     #we need to cr
-   else
-     #render text: 'Woah', status: 401
-     redirect_to '/'
-   end
-   # user = User.find_by(email: params[:email])
-   # puts User.authenticate 
-   # if user && user.authenticate(params[:password])
-   #   session[:user_id] = user.id 
-   # else
-   #   @message ="This email and password combination does not exist."
-   #   render :index
-   # end
- end
 
-  
-=======
+  def index
     if session[:user_id] != nil # the client is logged in
       @user = User.find(session[:user_id])
       # same as the one below just more abstract
@@ -39,26 +17,24 @@ class ItinerariesController < ApplicationController
     end
   end
 
-    def new
-      @itineraries = Itinerary.new
-      render :index
+  def new
+    @itineraries = Itinerary.new
+    render :index
+  end
+
+  def create
+    @itineraries = Itinerary.new
+    @itineraries.name = params[:name]
+
+    @itineraries.user_id = session[:user_id]
+
+    if @itineraries.save 
+      redirect_to '/itineraries'
     end
-
-    def create
-      @itineraries = Itinerary.new
-      @itineraries.name = params[:name]
-
-      @itineraries.user_id = session[:user_id]
+  end
 
 
-      
-
-      if @itineraries.save 
-        redirect_to '/itineraries'
-      end
->>>>>>> 6463a2d28edafa23173d2513a3e0fe511ebb4ff7
-
- def show
- end
+  def show
+  end
 end
 
